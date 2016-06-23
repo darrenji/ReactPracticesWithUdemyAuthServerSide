4a637926dd969f867e1fa6da4009029a530ecf4b
darren <764190362@qq.com>
2016-06-23 14:38:08 +0800

    finish08

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,32 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next){
-    res.send({success: 'true'});
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    //判断email是否存在
+    User.findOne({email: email }, function(err, existingUser){
+        if(err) { return next(err); }
+        
+        //如果email存在，就返回一个error
+        if(existingUser){
+            return res.status(422).send({error: 'Email is in use'});
+        }
+        
+        //如果email不存在，创建并保存用户信息
+        const  user = new User({
+            email: email,
+            password: password
+        });
+        
+        user.save(function(err){
+            if(err){ return next(err); }
+            
+            //并响应表明用户已经创建
+            res.json({ success: true });
+        });
+        
+        
+    });    
+    
 }
